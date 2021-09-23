e862895c84e49d50043045291838ddd83f6f0bc8
Josh Tonner <jtonner@iu.edu>
2021-09-23 11:01:46 -0400

    Debugging Starspawner

--- WS0G5_Project/Assets/Scripts/StarSpawner.cs
+++ WS0G5_Project/Assets/Scripts/StarSpawner.cs
@@ -26,16 +26,19 @@ public class StarSpawner : MonoBehaviour
         if (starSpawnCount == 0)
         {
             Instantiate(star, spawnPoint1.position, spawnPoint1.rotation);
+            Debug.Log("Star Spawned!");
             starSpawnCount++;
         }
         if (starSpawnCount == 1)
         {
             Instantiate(star, spawnPoint2.position, spawnPoint2.rotation);
+            Debug.Log("Star Spawned!");
             starSpawnCount++;
         }
         if (starSpawnCount == 2)
         {
             Instantiate(star, spawnPoint3.position, spawnPoint3.rotation);
+            Debug.Log("Star Spawned!");
             starSpawnCount++;
         }
     }
