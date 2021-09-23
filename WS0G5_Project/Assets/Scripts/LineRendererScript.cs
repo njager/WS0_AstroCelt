b7ad509a09a10293a281b2412f03d8dc7547a869
Josh Tonner <jtonner@iu.edu>
2021-09-23 19:33:17 -0400

    Added a line tag and renamed the script

--- WS0G5_Project/Assets/Scripts/LineRendererScript.cs
+++ WS0G5_Project/Assets/Scripts/LineRendererScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineRenderer : MonoBehaviour
+public class LineRendererScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
