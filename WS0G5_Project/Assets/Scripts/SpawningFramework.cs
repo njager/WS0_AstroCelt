f271d383b69b2d6fdd43d418b05b0a6041b2e4dc
Josh Tonner <jtonner@iu.edu>
2021-09-30 10:32:02 -0400

    Visual Studio corruption fix

--- WS0G5_Project/Assets/Scripts/SpawningFramework.cs
+++ WS0G5_Project/Assets/Scripts/SpawningFramework.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawningFramework : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
         
     }
