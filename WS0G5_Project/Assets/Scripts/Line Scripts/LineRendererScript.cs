9c2f63c18fad657aabdcd873d1caca6e3ea9196e
Josh Tonner <jtonner@iu.edu>
2021-10-07 08:10:39 -0400

    Capsule colliders added but not reporting

--- WS0G5_Project/Assets/Scripts/Line Scripts/LineRendererScript.cs
+++ WS0G5_Project/Assets/Scripts/Line Scripts/LineRendererScript.cs
@@ -5,28 +5,56 @@ using UnityEngine;
 public class LineRendererScript : MonoBehaviour
 {
     [Header("Self Reference")]
-    public LineRenderer lineRenderer;
+    public GameObject lineGameObject;
 
-    public bool lineDrew = true; 
+    public LineRenderer selfLine;
+    public float LineWidth = 0.5f; 
+    private CapsuleCollider capsule;
 
-    void OnCollisionEnter(Collision collision)
+
+    public bool lineDrew = true;
+
+    [Header("Capsule Collider")]
+    public Vector3 start;
+    public Vector3 end;
+
+    public void Start()
+    {
+        selfLine = lineGameObject.GetComponent<LineRenderer>();
+        capsule = lineGameObject.GetComponent<CapsuleCollider>(); 
+        capsule.radius = LineWidth / 2;
+        capsule.center = Vector3.zero;
+        capsule.direction = 2; // Z-axis for easier "LookAt" orientation
+    }
+
+    public void Update()
+    {
+        start = selfLine.GetPosition(0);
+        end = selfLine.GetPosition(1);
+
+        capsule.transform.position = start + (end - start) / 2;
+        capsule.transform.LookAt(start);
+        capsule.height = (end - start).magnitude;
+    }
+
+    void OnColliderEnter()
     {
         Debug.Log("Hit!");
-        GameObject other = collision.gameObject; 
-        if (other.CompareTag("Line"))
-        {
-            if (other != lineRenderer)
-            {
-                Debug.Log("Destroying"); 
-                lineDrew = false;
-                Destroy(lineRenderer);
-                return;
-            }
-        }
-        else
-        {
-            return; 
-        }
+        //GameObject other = collision.gameObject; 
+        //if (other.CompareTag("Line"))
+        //{
+        //    if (other != lineGameObject)
+       //     {
+    //          Debug.Log("Destroying"); 
+      //          lineDrew = false;
+        //        Destroy(lineGameObject);
+       //         return;
+         //   }
+      //  }
+      //  else
+     //   {
+     //       return; 
+    //    }
     }
     public void OnDestroy()
     {
