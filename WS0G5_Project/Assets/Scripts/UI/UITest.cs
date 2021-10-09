e71eec46e3672bb2d84f2c0fe59eedd2fbeadea1
njager <59936380+njager@users.noreply.github.com>
2021-10-09 16:08:37 -0400

    added variables to UI script

--- WS0G5_Project/Assets/Scripts/UI/UITest.cs
+++ WS0G5_Project/Assets/Scripts/UI/UITest.cs
@@ -6,6 +6,26 @@ using UnityEngine.UI;
 
 public class UITest : MonoBehaviour
 {
+    //public variables
+
+
+    //private variables
+    [Header("Variables")]
+    [SerializeField] int enemyCount;
+    [SerializeField] float timer;
+    [SerializeField] int ceCount;
+    [SerializeField] int playerHealth;
+    [SerializeField] int enemyHealth;
+
+    //UI variables
+    [Header("UI Element Slots")]
+    [SerializeField] TextMeshProUGUI ceCountText;
+    [SerializeField] TextMeshProUGUI enemyCountText;
+    [SerializeField] TextMeshProUGUI playerHealthText;
+    [SerializeField] TextMeshProUGUI enemyHealthText;
+    [SerializeField] TextMeshProUGUI timerText;
+
+
     // Start is called before the first frame update
     void Start()
     {
