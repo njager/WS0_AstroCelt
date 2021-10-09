5470af0b9ed162db2a5e84044c9bb8bb16f7ec2e
njager <59936380+njager@users.noreply.github.com>
2021-10-09 16:16:51 -0400

    scripted health and resource texts

--- WS0G5_Project/Assets/Scripts/UI/UITest.cs
+++ WS0G5_Project/Assets/Scripts/UI/UITest.cs
@@ -12,10 +12,13 @@ public class UITest : MonoBehaviour
     //private variables
     [Header("Variables")]
     [SerializeField] int enemyCount;
+    [SerializeField] int enemyMaxCount;
     [SerializeField] float timer;
     [SerializeField] int ceCount;
     [SerializeField] int playerHealth;
+    [SerializeField] int playerMaxHealth;
     [SerializeField] int enemyHealth;
+    [SerializeField] int enemyMaxHealth;
 
     //UI variables
     [Header("UI Element Slots")]
@@ -29,12 +32,23 @@ public class UITest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set up the text
+        SetText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //update the text
+        SetText();
+    }
+
+    //sets the text objects
+    void SetText()
+    {
+        enemyCountText.text = enemyCount.ToString() + "/" + enemyMaxCount.ToString();
+        ceCountText.text = ceCount.ToString();
+        playerHealthText.text = playerHealth.ToString() + "/" + playerMaxHealth.ToString();
+        enemyHealthText.text = enemyHealth.ToString() + "/" + enemyMaxHealth.ToString();
     }
 }
