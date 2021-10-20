dcf044de83f6c1ef16497730926703e6093795cd
Josh Tonner <jtonner@iu.edu>
2021-10-20 19:12:07 -0400

    Debug progress on star generation

--- WS0G5_Project/Assets/Scripts/ConstellationScripts/NewStarMapScript.cs
+++ WS0G5_Project/Assets/Scripts/ConstellationScripts/NewStarMapScript.cs
@@ -1,30 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro; 
 
 public class NewStarMapScript : MonoBehaviour
 {
     private GlobalController global;
+    public GameObject loadBar;
+    private bool loadBarBool; 
 
     void Start()
     {
-        global = GlobalController.instance; 
+        global = GlobalController.instance;
+        loadBar.SetActive(false);
+        loadBarBool = false; 
     }
 
     public void Update()
     {
-        
+        if (loadBarBool == true)
+        {
+
+        }
+        else
+        {
+            return; 
+        }
     }
 
     public void NewStars()
     {
+        LoadingScreen(); 
         Debug.Log("Loading in New Stars");
         ChangingStarMap(); 
     }
 
     public void LoadingScreen()
     {
+        // First Reset Variables
 
+
+        loadBar.SetActive(true); // Then put into scene
     }
 
     public void ChangingStarMap() 
