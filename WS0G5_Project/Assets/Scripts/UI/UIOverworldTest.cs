7074635be158262bef497f4683ba0d23f258aeab
njager <59936380+njager@users.noreply.github.com>
2021-10-27 23:46:06 -0400

    added script skeleton

--- WS0G5_Project/Assets/Scripts/UI/UIOverworldTest.cs
+++ WS0G5_Project/Assets/Scripts/UI/UIOverworldTest.cs
@@ -8,6 +8,21 @@ using DG.Tweening;
 
 public class UIOverworldTest : MonoBehaviour
 {
+    //private variables
+    [Header("Variables")]
+    [SerializeField] int levelsCount;
+    [SerializeField] int levelsMaxCount;
+    [SerializeField] int ceCount;
+    [SerializeField] int vitUpgCost;
+    [SerializeField] int constUpgCost;
+    [SerializeField] int ritOneUpgCost;
+    [SerializeField] int ritTwoUpgCost;
+
+    //UI variables
+    [Header("UI Element Slots")]
+    [SerializeField] TextMeshProUGUI ceCountText;
+    [SerializeField] TextMeshProUGUI levelCountText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +33,21 @@ public class UIOverworldTest : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //sets the text objects
+    void SetText()
+    {
+        /*enemyCountText.text = enemyCount.ToString() + "/" + enemyMaxCount.ToString();
+        ceCountText.text = ceCount.ToString();
+        playerHealthText.text = playerHealth.ToString() + "/" + playerMaxHealth.ToString();
+        enemyHealthText.text = enemyHealth.ToString() + "/" + enemyMaxHealth.ToString();
+
+        //update health bars
+        playerHealthBar.fillAmount = (float)playerHealth / (float)playerMaxHealth;
+        enemyHealthBar.fillAmount = (float)enemyHealth / (float)enemyMaxHealth;
+
+        //update charge bar
+        enemyChargeBar.fillAmount = (float)chargeTime / (float)maxCharge;*/
     }
 }
