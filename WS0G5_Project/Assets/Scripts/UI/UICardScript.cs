3684e088dd7e21e5a7563c0182b2e355977486c7
njager <59936380+njager@users.noreply.github.com>
2021-11-01 22:20:28 -0400

    tried debugging drag and drop

--- WS0G5_Project/Assets/Scripts/UI/UICardScript.cs
+++ WS0G5_Project/Assets/Scripts/UI/UICardScript.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class UICardScript : MonoBehaviour
 {
+    //private variables
+    //public EventTrigger dragger;
+    //bool dragging = EventSystem.dragging
+
+    //public variables
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //dragger = GetComponent<EventTrigger>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (dragger.dragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }*/
     }
 
     //detect when the card enters a slot trigger
@@ -27,4 +38,5 @@ public class UICardScript : MonoBehaviour
     {
         Debug.Log("Exiting the slot!");
     }
+
 }
