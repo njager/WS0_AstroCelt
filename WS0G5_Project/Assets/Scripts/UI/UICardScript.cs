5a03cee697f3a3fbda9e314f0ac0e2714ad1e486
njager <59936380+njager@users.noreply.github.com>
2021-11-05 00:51:48 -0400

    added mroe conditions

--- WS0G5_Project/Assets/Scripts/UI/UICardScript.cs
+++ WS0G5_Project/Assets/Scripts/UI/UICardScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class UICardScript : MonoBehaviour
 {
@@ -9,20 +10,26 @@ public class UICardScript : MonoBehaviour
     //public EventTrigger dragger;
     //bool dragging = EventSystem.dragging
     GameObject currentCard;
-    
+
+    [Header("Bools")]
+    [SerializeField] bool isDragging;
+    [SerializeField] bool isDamage;
+    [SerializeField] bool isHeal;
+    [SerializeField] bool isShield;
+    [SerializeField] bool inStarSlot1;
+    [SerializeField] bool inStarSlot2;
+
     //public variables
-    public bool isDragging;
-    public bool isDamage;
-    public bool isHeal;
-    public bool isShield;
-    public bool inStarSlot1;
-    public bool inStarSlot2;
+    public int damageStarLoadCount;
+    public int healStarLoadCount;
 
     // Start is called before the first frame update
     void Start()
     {
         //currentCard = GetComponent<GameObject>();
         //dragger = GetComponent<EventTrigger>();
+
+        //check tag of current card
         if(this.tag == "DamageCard")
         {
             isDamage = true;
@@ -36,6 +43,9 @@ public class UICardScript : MonoBehaviour
         {
             transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         }*/
+
+        //if mouse button has been clicked and is held down then dragging
+        //this is techincal debt because this does it any time mouse is clicked down, not just on card
         if (Input.GetMouseButtonDown(0))
         {
             isDragging = true;
@@ -43,6 +53,22 @@ public class UICardScript : MonoBehaviour
         else if (Input.GetMouseButtonUp(0))
         {
             isDragging = false;
+        }
+
+        //check if card is no longer dragging to see if it's in a star slot
+        if (!isDragging)
+        {
+            if (inStarSlot1)
+            {
+                if (isDamage)
+                {
+                    damageStarLoadCount++;
+                }
+            }
+        }
+        if (isDragging)
+        {
+            damageStarLoadCount = 0;
         }
 
     }
