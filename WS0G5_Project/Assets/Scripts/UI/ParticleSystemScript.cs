165119c8df71780c04872d1aa5a3a8ae4e041ed4
Josh Tonner <jtonner@iu.edu>
2021-11-09 08:45:45 -0500

    Particle System Script Fixed

--- WS0G5_Project/Assets/Scripts/UI/ParticleSystemScript.cs
+++ WS0G5_Project/Assets/Scripts/UI/ParticleSystemScript.cs
@@ -15,38 +15,53 @@ public class ParticleSystemScript : MonoBehaviour
     // These can also be used when clicking on the stars
     public void SpawnDamageParticleEffect(Transform _transform)
     {
-        Instantiate(damageParticlePrefab, _transform);
+        GameObject _particle = Instantiate(damageParticlePrefab, _transform);
+        ParticleSystem _system = _particle.GetComponent<ParticleSystem>();
+        _system.Play(); 
+
     }
 
     public void SpawnHealthParticleEffect(Transform _transform)
     {
-        Instantiate(damageParticlePrefab, _transform);
+        GameObject _particle = Instantiate(healthParticlePrefab, _transform);
+        ParticleSystem _system = _particle.GetComponent<ParticleSystem>();
+        _system.Play();
     }
 
     public void SpawnShieldParticleEffect(Transform _transform)
     {
-        Instantiate(shieldParticlePrefab, _transform);
+        GameObject _particle = Instantiate(shieldParticlePrefab, _transform);
+        ParticleSystem _system = _particle.GetComponent<ParticleSystem>();
+        _system.Play();
     }
 
     public void SpawnStunParticleEffect(Transform _transform)
     {
-        Instantiate(stunParticlePrefab, _transform);
+        GameObject _particle = Instantiate(stunParticlePrefab, _transform);
+        ParticleSystem _system = _particle.GetComponent<ParticleSystem>();
+        _system.Play();
     }
 
     public void SpawnBaseStarParticleEffect(Transform _transform)
     {
-        Instantiate(baseStarParticlePrefab, _transform);
+        GameObject _particle = Instantiate(baseStarParticlePrefab, _transform);
+        ParticleSystem _system = _particle.GetComponent<ParticleSystem>();
+        _system.Play();
     }
 
     // Not for being used in a constellation, but for when having clicked on a constellation 
     public void SpawnNodeParticleEffect(Transform _transform)
     {
-        Instantiate(nodeClickedParticlePrefab, _transform);
+        GameObject _particle = Instantiate(nodeClickedParticlePrefab, _transform);
+        ParticleSystem _system = _particle.GetComponent<ParticleSystem>();
+        _system.Play();
     }
 
     public void SpawnConfirmParticleEffect(Transform _transform)
     {
-        Instantiate(nodeClickedParticlePrefab, _transform);
+        GameObject _particle = Instantiate(confirmParticlePrefab, _transform);
+        ParticleSystem _system = _particle.GetComponent<ParticleSystem>();
+        _system.Play();
     }
 }
 
