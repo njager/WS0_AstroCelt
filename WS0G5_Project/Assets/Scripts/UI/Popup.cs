004ee87eafa1f81ece61b560177dc2fae2751425
njager <59936380+njager@users.noreply.github.com>
2021-11-23 18:47:15 -0500

    got popup new feedback working

--- WS0G5_Project/Assets/Scripts/UI/Popup.cs
+++ WS0G5_Project/Assets/Scripts/UI/Popup.cs
@@ -11,10 +11,15 @@ public class Popup : MonoBehaviour
     private GlobalController global;
     private TextMeshPro textMesh;
     private float disappearTimer;
+    private float destroyTimer = 1f;
     private Color textColor;
-    private const float DISAPPEAR_TIMER_MAX = 1f;
+    private const float DISAPPEAR_TIMER_MAX = 3f;
     private static int sortingOrder;
     [SerializeField] Vector3 enemyHealthPos;
+    [SerializeField] Vector3 playerHealthPos;
+    private bool isRed;
+    private bool isGreen;
+    private bool isBlue;
 
     //public variables
     public static GameObject pfPopupStatic;
@@ -34,6 +39,7 @@ public class Popup : MonoBehaviour
         global = GlobalController.instance;
         DOTween.Init();
         enemyHealthPos = new Vector3(21, 5, 0);
+        playerHealthPos = new Vector3(-21, 5, 0);
     }
 
     //create the popup at position with certain #
@@ -61,17 +67,30 @@ public class Popup : MonoBehaviour
         {
             //textMesh.fontSize = 36;
             textColor = UtilsClass.GetColorFromString("5ECC71");
+            isRed = false;
+            isGreen = true;
+            isBlue = false;
         }
         else if (colorIndex == 1)
         {
             textColor = UtilsClass.GetColorFromString("DD6666");
             //textMesh.fontSize = 36;
+            isRed = true;
+            isGreen = false;
+            isBlue = false;
         }
         textMesh.color = textColor;
         disappearTimer = DISAPPEAR_TIMER_MAX;
 
+        //put newer popups on top
         sortingOrder++;
         textMesh.sortingOrder = sortingOrder;
+
+        //add a box collider because it auto deletes when spawned
+        gameObject.AddComponent<BoxCollider>();
+
+        //reset the scale after tweening it in spawn
+        gameObject.transform.DOScale(new Vector3(1, 1, 0), 0.01f);
     }
 
     private void Update()
@@ -100,32 +119,75 @@ public class Popup : MonoBehaviour
         }*/
 
         
-        //MoveToBar();
-
+        //delay disappear for popup
         disappearTimer -= Time.deltaTime;
         if(disappearTimer < 0)
         {
-            float disappearSpeed = 3f;
-            textColor.a -= disappearSpeed * Time.deltaTime;
+            //float disappearSpeed = 3f;
+            //textColor.a -= disappearSpeed * Time.deltaTime;
             //textMesh.color = textColor;
-            textMesh.DOFade(0f, 3f);
-            
-            
-            if (textColor.a < 0)
+            textMesh.DOFade(0f, 1f);
+            destroyTimer -= Time.deltaTime;
+            if (destroyTimer < 0)
             {
                 Destroy(gameObject);
             }
+
+
+            /*if (textColor.a < 0)
+            {
+                Destroy(gameObject);
+            }*/
         }
     }
 
+    /* //this is an obsolete function replaced by coroutine
     public void MoveToBar()
     {
         gameObject.transform.DOMove(enemyHealthPos, 2f);
+    }*/
+
+    //wait then move popup to enemy health bar, with other punchy effects
+    IEnumerator MovePopup()
+    {
+        //punch the position for shake and scale
+        gameObject.transform.DOShakePosition(1f, 0.3f, 10, 10f);
+        gameObject.transform.DOScale(new Vector3(1.5f, 1.5f, 0), 0.2f);
+
+        //flash color then reset
+        textMesh.DOColor(UtilsClass.GetColorFromString("FFFFFF"), 0.2f);
+
+        yield return new WaitForSeconds(0.25f);
+
+        gameObject.transform.DOScale(new Vector3(1, 1, 0), .2f);
+
+        if (isGreen)
+        {
+            textMesh.DOColor(UtilsClass.GetColorFromString("5ECC71"), 0.2f);
+        }
+        else if (isRed)
+        {
+            textMesh.DOColor(UtilsClass.GetColorFromString("DD6666"), 0.2f);
+        }
+
+
+        yield return new WaitForSeconds(1);
+
+        if (isRed)
+        {
+            gameObject.transform.DOMove(enemyHealthPos, 2f);
+        }
+        else if (isGreen)
+        {
+            gameObject.transform.DOMove(playerHealthPos, 2f);
+        }
+        
     }
 
+    //if the popup enters the screen, move it
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Popup entered the screen!");
-        MoveToBar();
+        StartCoroutine("MovePopup");
     }
 }
